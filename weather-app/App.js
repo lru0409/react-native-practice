71b937869f145ca30b3123d71445e86623822afc
lru0409 <lru040409@gmail.com>
2025-04-18 11:33:10 +0900

    feat: display weather information based on user's location

--- weather-app/App.js
+++ weather-app/App.js
@@ -1,43 +1,89 @@
+import * as Location from "expo-location";
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { View, Text, Dimensions, StyleSheet, ScrollView } from "react-native";
-
+import React, { useState, useEffect } from "react";
+import { OPEN_WEATHER_API_KEY } from "@env";
+import {
+  View,
+  Text,
+  Dimensions,
+  StyleSheet,
+  ScrollView,
+  ActivityIndicator,
+} from "react-native";
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
 export default function App() {
+  const [city, setCity] = useState("Loading...");
+  const [days, setDays] = useState([]);
+  const [ok, setOk] = useState(true);
+
+  const getWeather = async () => {
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+    if (!granted) {
+      setOk(false);
+    }
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({
+      accuracy: Location.Accuracy.Highest,
+    });
+    const location = await Location.reverseGeocodeAsync({
+      latitude,
+      longitude,
+    });
+    setCity(location[0].city);
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${OPEN_WEATHER_API_KEY}&units=metric`
+    );
+    const json = await response.json();
+    setDays(json.list.filter(({ dt_txt }) => dt_txt.includes("00:00:00")));
+  };
+  useEffect(() => {
+    getWeather();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <View style={styles.city}>
-        <Text style={styles.cityName}>Seoul</Text>
-      </View>
-      <ScrollView
-        pagingEnabled
-        horizontal
-        showsHorizontalScrollIndicator={false}
-        contentContainerStyle={styles.weather}
-      >
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
+      {ok ? (
+        <>
+          <View style={styles.city}>
+            <Text style={styles.cityName}>{city}</Text>
+          </View>
+          <ScrollView
+            pagingEnabled
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            contentContainerStyle={styles.weather}
+          >
+            {days.length === 0 ? (
+              <View style={styles.day}>
+                <ActivityIndicator
+                  color="white"
+                  size="large"
+                  style={{ marginTop: 10 }}
+                />
+              </View>
+            ) : (
+              days.map((day) => (
+                <View style={styles.day} key={day.dt}>
+                  <Text style={styles.temp}>
+                    {parseFloat(day.main.temp).toFixed(1)}
+                  </Text>
+                  <Text style={styles.description}>{day.weather[0].main}</Text>
+                  <Text style={styles.tinyText}>
+                    {day.weather[0].description}
+                  </Text>
+                </View>
+              ))
+            )}
+          </ScrollView>
+        </>
+      ) : (
+        <View style={styles.errorContainer}>
+          <Text style={styles.error}>위치 권한을 부여해 주세요.</Text>
         </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-      </ScrollView>
-      <StatusBar style="light" />
+      )}
+      <StatusBar style="dark" />
     </View>
   );
 }
@@ -45,7 +91,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "tomato",
+    backgroundColor: "skyblue",
   },
   city: {
     flex: 1,
@@ -53,8 +99,8 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   cityName: {
-    fontSize: 68,
-    fontWeight: "500",
+    fontSize: 60,
+    fontWeight: "600",
   },
   weather: {},
   day: {
@@ -63,10 +109,22 @@ const styles = StyleSheet.create({
   },
   temp: {
     marginTop: 50,
-    fontSize: 178,
+    fontSize: 160,
+    fontWeight: "600",
   },
   description: {
-    marginTop: -30,
+    marginTop: -20,
     fontSize: 60,
+  },
+  tinyText: {
+    fontSize: 25,
+  },
+  errorContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  error: {
+    fontSize: 15,
   },
 });
