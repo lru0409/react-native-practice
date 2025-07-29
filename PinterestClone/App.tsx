b1eaa4a74e279bc8fb4bb3ee84256e2afed14410
lru0409 <lru040409@gmail.com>
2025-07-29 10:32:59 +0900

    feature: add bottom tab

--- PinterestClone/App.tsx
+++ PinterestClone/App.tsx
@@ -1,21 +1,69 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { NewAppScreen } from '@react-native/new-app-screen';
 import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/Ionicons';
+import HomeScreen from './src/screens/home';
+import SearchScreen from './src/screens/search';
+import CollectionScreen from './src/screens/collection';
+
+const Tab = createBottomTabNavigator();
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
-    <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
-    </View>
+    <SafeAreaProvider>
+      <View style={styles.container}>
+        <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+        <NavigationContainer>
+          <Tab.Navigator
+            screenOptions={{
+              headerShown: false,
+              tabBarLabelStyle: {
+                fontSize: 12,
+                marginTop: 3,
+                color: 'black',
+              },
+            }}
+          >
+            <Tab.Screen
+              name="홈"
+              component={HomeScreen}
+              options={{
+                tabBarIcon: ({ focused }) => (
+                  <Icon name={focused ? 'home' : 'home-outline'} size={24} />
+                ),
+              }}
+            />
+            <Tab.Screen
+              name="검색"
+              component={SearchScreen}
+              options={{
+                tabBarIcon: ({ focused }) => (
+                  <Icon
+                    name={focused ? 'search' : 'search-outline'}
+                    size={24}
+                  />
+                ),
+              }}
+            />
+            <Tab.Screen
+              name="저장됨"
+              component={CollectionScreen}
+              options={{
+                tabBarIcon: ({ focused }) => (
+                  <Icon
+                    name={focused ? 'person' : 'person-outline'}
+                    size={24}
+                  />
+                ),
+              }}
+            />
+          </Tab.Navigator>
+        </NavigationContainer>
+      </View>
+    </SafeAreaProvider>
   );
 }
 
