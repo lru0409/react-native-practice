2ddccf2bd37d5c54d2e2def1f1e794ba04b91151
lru0409 <lru040409@gmail.com>
2025-09-25 06:31:02 +0900

    fix: resolve eslint parsing errors and add env.d.ts for @env module

--- PinterestClone/babel.config.cjs
+++ PinterestClone/babel.config.cjs
@@ -6,6 +6,9 @@ module.exports = {
       {
         moduleName: '@env',
         path: '.env',
+        blacklist: null,
+        whitelist: null,
+        safe: false,
         allowUndefined: false,
       },
     ],
