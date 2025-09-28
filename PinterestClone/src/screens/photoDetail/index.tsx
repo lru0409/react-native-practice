0ef2cf2d5e59dd60a152f81f0eb5744a7bde6b98
lru0409 <lru040409@gmail.com>
2025-09-28 13:00:00 +0900

    refactor: handle loading state without AutoHeightImage in photoDetail

--- PinterestClone/src/screens/photoDetail/index.tsx
+++ PinterestClone/src/screens/photoDetail/index.tsx
@@ -1,23 +1,25 @@
 import { useRef } from 'react';
-import { View, Text, StyleSheet, Image, TouchableOpacity, ScrollView, NativeSyntheticEvent, NativeScrollEvent, ActivityIndicator } from 'react-native';
+import { View, Text, StyleSheet, Image, TouchableOpacity, ScrollView, NativeSyntheticEvent, NativeScrollEvent, ActivityIndicator, Dimensions } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useRoute, RouteProp, useNavigation } from '@react-navigation/native';
 
 import { UNSPLASH_ACCESS_KEY } from '@env';
 import { SCREEN_HORIZONTAL_PADDING } from '@src/constants/styles';
 import { RootStackParamList } from '@src/App';
-import AutoHeightImage from '@src/components/AutoHeightImage';
 import Icon from 'react-native-vector-icons/Ionicons';
 import PhotoGrid from '@src/components/PhotoGrid';
 import { useEffect, useState } from 'react';
 import { UNSPLASH_BASE_URL } from '@src/constants/api';
 import { Photo, PhotoResponse } from '@src/types/photo';
 
+const CONTAINER_WIDTH = Dimensions.get('window').width - SCREEN_HORIZONTAL_PADDING * 2;
+
 export default function PhotoDetailScreen() {
   const navigation = useNavigation();
   const { params } = useRoute<RouteProp<RootStackParamList, 'PhotoDetail'>>();
   const { photo } = params;
   const [isPhotoLoading, setIsPhotoLoading] = useState<boolean>(true);
+  const [photoHeight, setPhotoHeight] = useState<number>(0);
 
   const [relatedPhotos, setRelatedPhotos] = useState<Photo[]>([]);
   const [hasMoreRelated, setHasMoreRelated] = useState<boolean>(false);
@@ -49,7 +51,7 @@ export default function PhotoDetailScreen() {
     const totalPages = data.total_pages;
     setHasMoreRelated(relatedPageRef.current < totalPages);
     setRelatedPhotos(prev => [
-      ...prev,
+      ...(page === 1 ? [] : prev),
       ...data.results.map(
         item =>
           ({
@@ -67,15 +69,23 @@ export default function PhotoDetailScreen() {
   };
 
   useEffect(() => {
+    const readyPhoto = async () => {
+      setIsPhotoLoading(true);
+      const { width, height } = await Image.getSize(photo.urls.full);
+      const ratio = height / width;
+      setPhotoHeight(CONTAINER_WIDTH * ratio);
+      setIsPhotoLoading(false);
+    }
     const firstFetch = async () => {
       setIsFirstFetching(true);
+      relatedPageRef.current = 1;
       await fetchRelatedPhotos(relatedPageRef.current, photo.description);
       setIsFirstFetching(false);
     };
-    firstFetch();
-  }, []);
 
-  useEffect(() => {
+    readyPhoto();
+    firstFetch();
+
     if (scrollRef.current) {
       scrollRef.current.scrollTo({ y: 0, animated: true });
     }
@@ -111,15 +121,20 @@ export default function PhotoDetailScreen() {
 
   return (
     <SafeAreaView style={styles.container} edges={['top']}>
-      <View style={{ height: '100%' }}>
+      <View style={{ height: '100%'}}>
         <TouchableOpacity style={styles.backButton} onPress={() => navigation.goBack()}>
           <Icon name="chevron-back" size={20} style={styles.backIcon} />
         </TouchableOpacity>
-        <ScrollView ref={scrollRef} style={styles.contentContainer} onScroll={handleScroll}>
-          <AutoHeightImage uri={photo.urls.full} onReady={() => setIsPhotoLoading(false)} />
 
-          {!isPhotoLoading && (
-            <>
+        {isPhotoLoading && (
+          <View style={styles.initialLoadingContainer}>
+            <ActivityIndicator />
+          </View>
+        )}
+
+        {!isPhotoLoading && (
+          <ScrollView ref={scrollRef} style={styles.contentContainer} onScroll={handleScroll}>
+            <Image source={{ uri: photo.urls.full }} style={[styles.photo, { height: photoHeight }]} />
               <View style={styles.infoContainer}>
                 <Text style={styles.description}>{photo.description}</Text>
                 <View style={styles.dateContainer}>
@@ -141,17 +156,14 @@ export default function PhotoDetailScreen() {
                     <ActivityIndicator />
                   </View>
                 ) : <PhotoGrid photos={relatedPhotos} />}
-              </View>
-            </>
-          )}
-
-          {isFetchingMore && (
-            <View style={styles.listLoadingContainer}>
-              <ActivityIndicator />
-            </View>
-          )}
-
-        </ScrollView>
+              </View>  
+              {isFetchingMore && (
+                <View style={styles.listLoadingContainer}>
+                  <ActivityIndicator />
+                </View>
+              )}
+          </ScrollView>
+        )}
       </View>
     </SafeAreaView>
   );
@@ -161,16 +173,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingHorizontal: SCREEN_HORIZONTAL_PADDING,
-  },
-  initialLoadingContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  listLoadingContainer: {
-    height: 60,
-    justifyContent: 'center',
-    alignItems: 'center',
   },
   contentContainer: {
     height: '100%',
@@ -188,6 +190,21 @@ const styles = StyleSheet.create({
   },
   backIcon: {
     margin: 'auto',
+  },
+  initialLoadingContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  listLoadingContainer: {
+    height: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  photo: {
+    width: CONTAINER_WIDTH,
+    resizeMode: 'cover',
+    borderRadius: 16,
   },
   infoContainer: {
     padding: 15,
