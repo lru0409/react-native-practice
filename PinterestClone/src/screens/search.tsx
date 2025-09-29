cd1d5b898ef4c8e476708c50cca83c4dda86d686
lru0409 <lru040409@gmail.com>
2025-09-29 19:47:11 +0900

    feat: add textInput to search screen

--- PinterestClone/src/screens/search.tsx
+++ PinterestClone/src/screens/search.tsx
@@ -1,12 +1,42 @@
-import { View, Text } from 'react-native';
+import { useState } from 'react';
+import { TextInput, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
+import { SCREEN_HORIZONTAL_PADDING } from '@src/constants/styles';
+
 export default function SearchScreen() {
+  const [query, setQuery] = useState('');
+
   return (
-    <SafeAreaView>
-      <View>
-        <Text>Search</Text>
-      </View>
+    <SafeAreaView style={styles.container}>
+      <TextInput
+        style={styles.textInput}
+        placeholder='Pinterest 검색'
+        placeholderTextColor='gray'
+        value={query}
+        onChangeText={setQuery}
+        returnKeyType='search'
+        onSubmitEditing={() => {
+          console.log('submit');
+        }}
+      />
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: SCREEN_HORIZONTAL_PADDING,
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: 'black',
+    borderRadius: 14,
+    borderStyle: 'solid',
+    paddingVertical: 13,
+    paddingHorizontal: 17,
+    fontSize: 15,
+    lineHeight: 18,
+  },
+})
