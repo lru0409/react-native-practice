e2a73cbb504db27c20f263ab719af8dd4ee27fb6
lru0409 <lru040409@gmail.com>
2025-10-26 19:12:29 +0900

    feat: implement OAuth token exchange and secure storage

--- SeaOfPhotos/src/screens/login/index.tsx
+++ SeaOfPhotos/src/screens/login/index.tsx
@@ -1,7 +1,7 @@
 import { View, Button, SafeAreaView, Linking } from 'react-native';
-// import EncryptedStorage from 'react-native-encrypted-storage';
+import EncryptedStorage from 'react-native-encrypted-storage';
 
-import { UNSPLASH_ACCESS_KEY } from '@env';
+import { UNSPLASH_ACCESS_KEY, UNSPLASH_SECRET_KEY } from '@env';
 import styles from './styles';
 import { useEffect } from 'react';
 
@@ -16,12 +16,32 @@ export default function LoginScreen() {
   }
 
   useEffect(() => {
-    const handleUrl = (event: { url: string }) => {
+    const handleUrl = async (event: { url: string }) => {
       const { url } = event;
       if (url.startsWith('com.seaofphotos://oauth')) {
         const code = url.split('code=')[1];
         console.log('Authorization code:', code);
         // TODO: 토큰 발급 요청
+        const response = await fetch(`https://unsplash.com/oauth/token`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+          },
+          body: new URLSearchParams({
+            client_id: UNSPLASH_ACCESS_KEY,
+            client_secret: UNSPLASH_SECRET_KEY,
+            redirect_uri: 'com.seaofphotos://oauth',
+            code: code,
+            grant_type: 'authorization_code',
+          }).toString(),
+        });
+        if (!response.ok) {
+          const errText = await response.text();
+          console.log(`Failed to get token: ${errText}`);
+          return;
+        }
+        const data = await response.json();
+        await EncryptedStorage.setItem('unsplash_access_token', data.access_token);
       }
     }
     const subscription = Linking.addEventListener('url', handleUrl);
