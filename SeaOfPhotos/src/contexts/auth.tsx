86239106240d49c38d7c2bb6ee07877a17f167be
lru0409 <lru040409@gmail.com>
2025-10-29 22:00:00 +0900

    feat: add access token validation for check login

--- SeaOfPhotos/src/contexts/auth.tsx
+++ SeaOfPhotos/src/contexts/auth.tsx
@@ -1,32 +1,47 @@
 import { useState, useEffect, createContext, useContext } from 'react';
 import EncryptedStorage from 'react-native-encrypted-storage';
 
+import { UNSPLASH_BASE_URL } from '@src/constants/api';
+
 type AuthContextType = {
-    isLoggedIn: boolean | null;
-    setIsLoggedIn: (value: boolean) => void;
-}
+  isLoggedIn: boolean | null;
+  checkLogin: () => Promise<void>;
+};
 
 const AuthContext = createContext<AuthContextType | null>(null);
 
 export function AuthProvider({ children }: { children: React.ReactNode }) {
   const [isLoggedIn, setIsLoggedIn] = useState<boolean | null>(null);
 
-  useEffect(() => {
-    const checkLogin = async () => {
-      try {
-        const accessToken = await EncryptedStorage.getItem('unsplash_access_token');
-        setIsLoggedIn(!!accessToken);
-      } catch (error) {
-        console.error('Auth check error:', error);
-        setIsLoggedIn(false);
-      }
-    };
+  const validateAccessToken = async (accessToken: string) => {
+    const response = await fetch(`${UNSPLASH_BASE_URL}/me`, {
+      method: 'GET',
+      headers: { Authorization: `Bearer ${accessToken}`}
+    });
+    return response.ok;
+  }
 
+  const checkLogin = async () => {
+    const accessToken = await EncryptedStorage.getItem('unsplash_access_token');
+    if (!accessToken) {
+      setIsLoggedIn(false);
+      return;
+    }
+    const isValid = await validateAccessToken(accessToken);
+    if (!isValid) {
+      console.error('Invalid access token');
+      setIsLoggedIn(false);
+      return;
+    }
+    setIsLoggedIn(true);
+  };
+
+  useEffect(() => {
     checkLogin();
   }, []);
 
   return (
-    <AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn }}>
+    <AuthContext.Provider value={{ isLoggedIn, checkLogin }}>
       {children}
     </AuthContext.Provider>
   );
