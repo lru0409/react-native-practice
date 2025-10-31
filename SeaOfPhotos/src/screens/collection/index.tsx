d0c553790a1d94f5047c4f119524a41c805a9e23
lru0409 <lru040409@gmail.com>
2025-10-31 22:00:00 +0900

    feat: add temp logout button

--- SeaOfPhotos/src/screens/collection/index.tsx
+++ SeaOfPhotos/src/screens/collection/index.tsx
@@ -1,11 +1,20 @@
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import EncryptedStorage from 'react-native-encrypted-storage';
+
+import { useAuth } from '@src/contexts/auth';
 
 export default function CollectionScreen() {
+  const { checkLogin } = useAuth();
+
   return (
     <SafeAreaView>
       <View>
         <Text>Collection</Text>
+        <Button title="Logout" onPress={() => {
+          EncryptedStorage.removeItem('unsplash_access_token');
+          checkLogin();
+        }} />
       </View>
     </SafeAreaView>
   );
