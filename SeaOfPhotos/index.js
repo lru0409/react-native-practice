f0b5b2451d01779fa1c1c35fb5c85b84f837041a
lru0409 <lru040409@gmail.com>
2025-11-23 14:31:58 +0900

    chore: react-query setup

--- SeaOfPhotos/index.js
+++ SeaOfPhotos/index.js
@@ -3,7 +3,23 @@
  */
 
 import { AppRegistry } from 'react-native';
+import { useState } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
 import App from './src/App';
 import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+const Root = () => {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <App />
+      {/* 개발 시만 활성화, 프로덕션에서는 제거 가능 */}
+      {__DEV__ && <ReactQueryDevtools initialIsOpen={false} />}
+    </QueryClientProvider>
+  )
+};
+
+AppRegistry.registerComponent(appName, () => Root);
