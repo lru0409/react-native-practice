4d0e86f371178706cdda1a415c03558850278014
lru0409 <lru040409@gmail.com>
2025-11-24 21:23:43 +0900

    chore: remove ReactQueryDevtools

--- SeaOfPhotos/index.js
+++ SeaOfPhotos/index.js
@@ -5,7 +5,6 @@
 import { AppRegistry } from 'react-native';
 import { useState } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import App from './src/App';
 import { name as appName } from './app.json';
@@ -16,8 +15,6 @@ const Root = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <App />
-      {/* 개발 시만 활성화, 프로덕션에서는 제거 가능 */}
-      {__DEV__ && <ReactQueryDevtools initialIsOpen={false} />}
     </QueryClientProvider>
   )
 };
