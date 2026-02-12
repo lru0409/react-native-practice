26a3693e5c2941cdf4c6f7ac841de579ede77901
lru0409 <lru040409@gmail.com>
2026-02-12 22:00:00 +0900

    feat: markup collection detail screen

--- SeaOfPhotos/src/screens/collectionDetail/index.tsx
+++ SeaOfPhotos/src/screens/collectionDetail/index.tsx
@@ -1,16 +1,21 @@
-import { Text } from 'react-native';
+import { Text, View, ActivityIndicator, TouchableOpacity } from 'react-native';
 import { useRoute, RouteProp } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/AntDesign';
 
 import { RootStackParamList } from '@src/App';
-import { Container } from '@src/components';
+import { BottomDetectScrollView, Container, PhotoGrid } from '@src/components';
+import { usePagination } from '@src/hooks/usePagination';
+import { Photo } from '@src/types/photo';
 import CollectionService from '@src/services/collection';
+import formatDate from '@src/utils/formatDate';
+import styles from './styles';
 
 export default function CollectionDetailScreen() {
   const { params } = useRoute<RouteProp<RootStackParamList, 'CollectionDetail'>>();
   const { collection } = params;
 
   const {
-    data: collectionPhotos,
+    data: collectionPhotos, // NOTE: API를 통해 접근 가능한 사진만 받아올 수 있음
     isFetchingFirst: isCollectionPhotosLoading,
     isFetchingMore: isCollectionPhotosLoadingMore,
     isError: isCollectionPhotosError,
@@ -24,13 +29,29 @@ export default function CollectionDetailScreen() {
     <Container
       useHeader={true}
       headerTitle={collection.title}
+      headerRight={
+        <TouchableOpacity
+          style={styles.editButton}
+          onPress={() => console.log('edit collection')}
+        >
+          <Icon name="edit" size={20} style={styles.editIcon} />
+        </TouchableOpacity>
+      }
+      isLoading={isCollectionPhotosLoading}
+      isError={Boolean(isCollectionPhotosError)}
     >
-      <Text>description: {collection.description}</Text>
-      <Text>createdAt: {collection.createdAt}</Text>
-      <Text>updatedAt: {collection.updatedAt}</Text>
-      <Text>totalPhotos: {collection.totalPhotos}</Text>
-      <Text>private: {collection.private}</Text>
-      <Text>coverPhoto: {collection.coverPhoto.urls.small}</Text>
+      <BottomDetectScrollView onEndReached={() => loadMore()}>
+        <View style={styles.textContent}>
+          {collection.description ? (<Text style={styles.description}>{collection.description}</Text>) : null}
+          <Text style={styles.date}>Created {formatDate(collection.createdAt)} · Updated {formatDate(collection.updatedAt)}</Text>
+        </View>
+        <PhotoGrid photos={collectionPhotos} />
+        {isCollectionPhotosLoadingMore && (
+          <View style={styles.listLoadingContainer}>
+            <ActivityIndicator />
+          </View>
+        )}
+      </BottomDetectScrollView>
     </Container>
   );
 }
--- SeaOfPhotos/src/screens/collectionDetail/styles.ts
+++ SeaOfPhotos/src/screens/collectionDetail/styles.ts
@@ -1,6 +1,29 @@
 import { StyleSheet } from 'react-native';
 
+import commonStyles from '@src/styles/common';
+
 const styles = StyleSheet.create({
+  listLoadingContainer: commonStyles.listLoadingContainer,
+  editButton: {
+    height: 35,
+    width: 32,
+  },
+  editIcon: {
+    margin: 'auto',
+  },
+  textContent: {
+    paddingTop: 10,
+    paddingHorizontal: 15,
+    paddingBottom: 17,
+    gap: 10,
+  },
+  description: {
+    lineHeight: 20,
+  },
+  date: {
+    fontSize: 13,
+    color: 'gray',
+  }
 });
 
 export default styles;
