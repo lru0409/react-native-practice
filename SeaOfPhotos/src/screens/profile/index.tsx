aa825e8e83bbf01826c02ea60a4d1eb5aa4f9bbb
lru0409 <lru040409@gmail.com>
2026-02-22 13:00:51 +0900

    feat: markup profile screen

--- SeaOfPhotos/src/screens/profile/index.tsx
+++ SeaOfPhotos/src/screens/profile/index.tsx
@@ -1,25 +1,45 @@
-import { View, Text } from 'react-native';
+import { View, Text, Image, Button, TouchableOpacity } from 'react-native';
 import { useRoute, RouteProp } from '@react-navigation/native';
 
 import { RootStackParamList } from '@src/App';
 import { Container } from '@src/components';
+import AuthService from '@src/services/auth';
+import { useAuth } from '@src/contexts/auth';
+import styles from './styles';
 
 export default function ProfileScreen() {
   const { params } = useRoute<RouteProp<RootStackParamList, 'Profile'>>();
   const { user } = params;
 
-  console.log(user);
+  const { checkLogin } = useAuth();
 
   return (
     <Container
       useHeader={true}
       headerTitle='Profile'
+      edges={['top', 'right', 'bottom', 'left']}
     >
-      <View>
-        <Text>{user.username}</Text>
-        <Text>{user.name}</Text>
-        <Text>{user.email}</Text>
-        <Text>{user.profileImage.medium}</Text>
+      <View style={styles.container}>
+        <View style={styles.content}>
+          <View style={styles.profileContainer}>
+            <Image style={styles.profileImage} source={{ uri: user.profileImage.medium}} />
+            <View>
+              <Text style={styles.name}>{user.name}</Text>
+              <Text style={styles.username}>{user.username}</Text>
+              <Text style={styles.email}>{user.email}</Text>
+            </View>
+          </View>
+        </View>
+        {/* TODO: Container.Bottom 만들어서 사용 */}
+        <TouchableOpacity
+          style={styles.logoutButton}
+          onPress={() => {
+            AuthService.logout();
+            checkLogin();
+          }}
+        >
+          <Text style={styles.logoutButtonText}>Logout</Text>
+        </TouchableOpacity>
       </View>
     </Container>
   );
--- SeaOfPhotos/src/screens/profile/styles.ts
+++ SeaOfPhotos/src/screens/profile/styles.ts
@@ -3,7 +3,50 @@ import { StyleSheet } from 'react-native';
 import commonStyles from '@src/styles/common';
 
 const styles = StyleSheet.create({
-
+  container: {
+    flex: 1,
+    margin: 10,
+    marginBottom: 25,
+  },
+  content: {
+    flex: 1,
+  },
+  profileContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 15,
+  },
+  profileImage: {
+    width: 60,
+    height: 60,
+    borderRadius: '50%',
+  },
+  name: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  username: {
+    fontSize: 14,
+    color: 'gray',
+    marginBottom: 2,
+  },
+  email: {
+    fontSize: 14,
+    color: 'gray',
+  },
+  // TODO: login button과 스타일이 겹침. Button 컴포넌트 만들자
+  logoutButton: {
+    padding: 12,
+    borderRadius: 5,
+    backgroundColor: 'black',
+  },
+  logoutButtonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
 });
 
 export default styles;
