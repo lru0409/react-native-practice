df40da22253888ba276dddd03d3b45c8d954be3d
lru0409 <lru040409@gmail.com>
2026-03-14 12:58:11 +0900

    refactor: remove PhotoGrid and InfiniteScrollView components

--- SeaOfPhotos/src/components/index.ts
+++ SeaOfPhotos/src/components/index.ts
@@ -1,23 +1,19 @@
 import BackButton from './BackButton';
 import Button from './Button';
-import InfiniteScrollView from './InfiniteScrollView';
 import CollectionCard from './CollectionCard';
 import Container from './Container';
 import ErrorNotice from './ErrorNotice';
 import LikeButton from './LikeButton';
-import PhotoGrid from './PhotoGrid';
 import PhotoCard from './PhotoCard';
 import SearchInput from './SearchInput';
 
 export {
   BackButton,
   Button,
-  InfiniteScrollView,
   CollectionCard,
   Container,
   ErrorNotice,
   LikeButton,
-  PhotoGrid,
   PhotoCard,
   SearchInput,
 };
