0eb0b82c0166312b4ec9986d17e47399c2c97147
lru0409 <lru040409@gmail.com>
2026-03-31 20:43:08 +0900

    feat: add useUpdateMe hook

---
+++ SeaOfPhotos/src/hooks/useUpdateMe.ts
@@ -0,0 +1,39 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+
+import { MeService } from '@src/services';
+
+type UpdateMeParams = {
+  username?: string;
+  firstName?: string;
+  lastName?: string;
+  email?: string;
+  url?: string;
+  location?: string;
+  bio?: string;
+  instagramUsername?: string;
+};
+
+type UseUpdateMeOptions = {
+  onSuccess?: () => void;
+  onError?: (error: Error) => void;
+};
+
+export function useUpdateMe(options?: UseUpdateMeOptions) {
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation({
+    mutationFn: (params: UpdateMeParams) => MeService.updateMe(params),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['me'] });
+      options?.onSuccess?.();
+    },
+    onError: (error: Error) => {
+      options?.onError?.(error);
+    },
+  });
+
+  return {
+    updateMe: mutation.mutate,
+    isPending: mutation.isPending,
+  };
+}
